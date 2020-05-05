630e3915f250b1e1f02dd76ceca4ca96ffd87741
doantuyen15 <64451486+doantuyen15@users.noreply.github.com>
2020-05-05 15:29:55 +0700

    update activity

--- app/src/main/java/com/exercises/textgame/LoginActivity.kt
+++ app/src/main/java/com/exercises/textgame/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.exercises.textgame
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.login.*
 
 
 class LoginActivity : AppCompatActivity() {
@@ -9,5 +12,13 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+        validate.setOnClickListener{
+            val email = Email.text.toString()
+            val password = Password.text.toString()
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    Toast.makeText(this, "Successful", Toast.LENGTH_LONG)
+                }
+        }
     }
 }
